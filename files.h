b6b61f19931c29df605ce330d84a8ba3d225fdad
Amine Ennasri <71757860+EnnasriAmine@users.noreply.github.com>
2020-12-07 15:53:53 +0100

    Update files.h

--- files.h
+++ files.h
@@ -1,8 +1,8 @@
 #ifndef DEF_FILES
 #define DEF_FILES
 
-    int loadLevel(int level[][NB_BLOCS_HEIGHT]);
-    int saveLevel(int level[][NB_BLOCS_HEIGHT]);
+    int loadLevel(int level[][NB_BLOCKS_HEIGHT]);
+    int saveLevel(int level[][NB_BLOCKS_HEIGHT]);
 
 #endif
 
