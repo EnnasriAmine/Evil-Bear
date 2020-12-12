ee6395b5e82eb1b4b64b3066cd4ac546a474afb5
ihsanelagmiri <53486671+ihsanelagmiri@users.noreply.github.com>
2020-12-12 12:34:02 +0100

    Add files via upload

---
+++ editor.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+
+#include "constants.h"
+#include "editor.h"
+#include "files.h"
+
+void editor(SDL_Surface* screen)
+{
+    SDL_Surface *wall = NULL, *recipe = NULL, *flame = NULL, *bear = NULL;
+    SDL_Rect position;
+    SDL_Event event;
+
+    int go_on = 1;
+	int LeftclickOngoing = 0;
+	int RightclickOngoing = 0;
+    int currentObject = WALL,
+	int i = 0, j = 0;
+    int map[NB_BLOCKS_WIDTH][NB_BLOCS_HEIGHT] = {0};
+
+    wall = IMG_Load("wall.png");
+    recipe = IMG_Load("recipe.png");
+    flame = IMG_Load("flame.png");
+    bear = IMG_Load("bear_down.png");
+
+}
---
+++ editor.h
@@ -0,0 +1,6 @@
+#ifndef DEF_EDITOR
+#define DEF_EDITOR
+
+    void editor(SDL_Surface* screen);
+
+#endif
