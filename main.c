ef81f153f00b7557f52bddef61e4cff38aa22f84
Amine Ennasri <71757860+EnnasriAmine@users.noreply.github.com>
2020-12-13 18:08:58 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+
+#include "constants.h"
+#include "game.h"
+#include "editor.h"
+
+
+
+int main(int argc, char *argv[])
+{
+  
+}
