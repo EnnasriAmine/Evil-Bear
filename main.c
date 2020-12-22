c028d95a9cf5ba159362daead9f98fe9e9615dcf
Amine Ennasri <71757860+EnnasriAmine@users.noreply.github.com>
2020-12-22 12:51:38 +0100

    Update main.c

--- main.c
+++ main.c
@@ -11,5 +11,20 @@
 
 int main(int argc, char *argv[])
 {
+  SDL_Surface *screen = NULL, *menu = NULL;
+  SDL_Rect menuPosition;
+  SDL_Event event;
+
+  int go_on = 1;
+
+  SDL_Init(SDL_INIT_VIDEO);
+
+  SDL_WM_SetIcon(IMG_Load("recipe.png"), NULL);
+  screen = SDL_SetVideoMode(WIDTH_WINDOW, HEIGHT_WINDOW, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
+  SDL_WM_SetCaption("Evil Bear", NULL);
+
+  menu = IMG_Load("menu.png");
+  menuPosition.x = 0;
+  menuPosition.y = 0;
   
 }
